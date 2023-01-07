c7dce963fe069a6d1040920cbbd6b53cf7a34232
testwebportalmercedes <testwebportalmercedes@gmail.com>
2023-01-07 21:10:00 +0300

    Несколько негативных тестов для регистрации

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -48,10 +48,29 @@ class TestUserRegister(BaseCase):
         assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", f'Unexpected response content{response.content}'
 
 
-    def test_create_user_with_incorect_email(self):
+    def test_create_user_with_incorect_email(self):                              # Негативный тест что юзер без @ не может быть создан
         email = 'vinkotov+2example.com'
         data = self.test_user_register(email)
         response1 = MyRequests.post('/user/', data=data)
         Assertions.assert_code_status(response1, 400)
         Assertions.assert_not_text_in_response(response1, 'Invalid email format')
         print(response1.text)
+
+    def test_create_user_with_incorect_firstname(self):                          # Негативный тест что юзер > 250 символов не может быть создан
+        base_path = 'learnqa'
+        domain = 'example.com'
+        random_part = datetime.now().strftime('%m%d%y%H%M%S')
+        email = f'{base_path}{random_part}@{domain}'
+        username = 'l' *251
+        data = {
+
+            'username': username,
+            'firstName': 'learnqa1',
+            'lastName': 'learnqa1',
+            'email': email,
+            'password': '123'
+        }
+        response1 = MyRequests.post('/user/', data=data)
+        Assertions.assert_code_status(response1, 400)
+        Assertions.assert_not_text_in_response(response1, 'The value of "username" field is too long')
+
