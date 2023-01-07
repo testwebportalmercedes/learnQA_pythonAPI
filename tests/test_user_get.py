7ffd566de5beaabf2d67fc3d6843075e36e4c445
testwebportalmercedes <testwebportalmercedes@gmail.com>
2023-01-07 21:41:07 +0300

    Негативный тест для получения данных незарегистрированного пользователя

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -34,4 +34,23 @@ class TestUserGet(BaseCase):
         Assertions.assert_id_in_content(response2,'lastName')
 
 
+    def test_get_user_details_auth_not_auth_id(self):             # Тест для проверки авторизации с одного аккаунта и запроса другого
 
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = MyRequests.post('/user/login', data=data)       # Заменено функцией из My_Requeste
+        auth_sid = self.get_cookie(response1, 'auth_sid')
+        token = self.get_header(response1, 'x-csrf-token')
+        user_id_from_auth_metod = self.get_json_value(response1, 'user_id')
+
+        response2 = MyRequests.get(f'/user/58432', # Заменено функцией из My_Requeste
+)
+        print(response2.text)
+
+        Assertions.assert_id_in_content(response2, 'username')
+        Assertions.assert_not_id_in_content(response2, 'email')
+        Assertions.assert_not_id_in_content(response2, 'firstName')
+        Assertions.assert_not_id_in_content(response2, 'lastName')
